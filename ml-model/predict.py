21c8b1036f5e17b28af6e911be8de6a6e2af9488
Akshay Mehta <nkanal38@gmail.com>
2026-03-20 22:09:17 +0530

    fix model path for render

--- ml-model/predict.py
+++ ml-model/predict.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
+import os
 import joblib
 import numpy as np
 
 app = Flask(__name__)
 
 # Load trained model
-model = joblib.load("credit_model.pkl")
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "credit_model.pkl")
+model = joblib.load(model_path)
 
 @app.route("/predict", methods=["POST"])
 def predict():
